d253accffe4f1ef66c9e960a3cc0313c8bd955b4
Alexey Bykov <bkalex94@gmail.com>
2023-08-05 19:22:11 +0100

    Update shortcuts.sh

--- shortcuts.sh
+++ shortcuts.sh
@@ -11,12 +11,22 @@ profiling() {
   fi
 }
 
-update_properties() {
- # https://github.com/aosp-mirror/platform_packages_apps_settings/blob/f8ad32823c5709348a5fb9618166b07394e88d99/src/com/android/settings/development/DevelopmentSettings.java#L2857
- adb shell service call activity 1599295570
+bounds() {
+  current_value=$(adb shell getprop debug.layout true | tr -d '\r')
+  if [[ "$current_value" == "false" ]]; then     	
+    adb shell setprop debug.layout true
+    update_properties
+  else
+    adb shell setprop debug.layout false
+    update_properties
+  fi
 }
 
 screenshot() {
- adb exec-out screencap -p > screenshot.png
- mv -f screenshot.png ~/Desktop/screenshot.png
+  adb exec-out screencap -p > ~/Desktop/screenshot.png
+}
+
+update_properties() {
+ # https://github.com/aosp-mirror/platform_packages_apps_settings/blob/f8ad32823c5709348a5fb9618166b07394e88d99/src/com/android/settings/development/DevelopmentSettings.java#L2857
+ adb shell service call activity 1599295570
 }
